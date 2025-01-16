833ebdde1170a0885c64754f626e3ed81dfd0b84
HandsOnDevOpsTraining <129347802+HandsOnDevOpsTraining@users.noreply.github.com>
2025-01-16 20:37:24 +1100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "ami_value" {
+    description = "value for the ami"
+}
+
+variable "instance_type_value" {
+    description = "value for instance_type"
+}
+
+variable "region" {
+    description = "US East 2 - Ohio Region"
+}
